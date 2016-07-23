e53b036f11cfc5f47ee5d332192350c5229f5e67
dqhieu13 <dqhieu13@gmail.com>
2016-07-23 10:47:54 +0700

    Update awesome login screen :3

--- Podfile
+++ Podfile
@@ -8,4 +8,7 @@ target 'Twitteralization' do
   # Pods for Twitteralization
   pod ‘AFNetworking’, ‘3.0.0’
   pod ‘BDBOAuth1Manager’, ‘2.0.0’
+  pod ‘PulsingHalo’
+  pod ‘SVProgressHUD’
+
 end
--- Twitteralization/LoginViewController.swift
+++ Twitteralization/LoginViewController.swift
@@ -8,29 +8,53 @@
 
 import UIKit
 import BDBOAuth1Manager
+import PulsingHalo
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initHalo()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func initHalo() {
+        let halo = PulsingHaloLayer()
+        halo.haloLayerNumber = 2
+        halo.radius = self.view.frame.width / 2
+        halo.backgroundColor = UIColor.whiteColor().CGColor
+        halo.position = view.center
+        view.layer.addSublayer(halo)
+        halo.start()
     }
-
+    
     @IBAction func onLogin(sender: UIButton) {
+        showLoadingProgress(nil)
         TwitterClient.sharedInstance.login({ () -> () in
             print("I have logged in")
+            self.hideLoadingProgress()
             self.performSegueWithIdentifier("segueLogin", sender: nil)
         }) { (error: NSError) in
+            self.hideLoadingProgress()
             print(error.localizedDescription)
         }
         
         
     }
 
+    func showLoadingProgress(text: String?) {
+        if let text = text {
+            SVProgressHUD.showWithStatus(text)
+        }
+        else {
+            SVProgressHUD.show()
+        }
+        
+    }
+    
+    func hideLoadingProgress() {
+        SVProgressHUD.dismiss()
+    }
+    
 }
 
